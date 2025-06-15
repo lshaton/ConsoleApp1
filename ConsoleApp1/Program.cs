a583d87a3563cd28006199f7ea99821b48fedd68
Leo <lshaton@msn.com>
2025-06-15 20:13:58 +0300

    "Hello World2!"

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World1!");
+            Console.WriteLine("Hello World2!");
             Console.ReadLine();
         }
     }
